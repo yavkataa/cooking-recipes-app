87274a196c5259aef4864bd1715fc8d34168abb2
Yavor Yankov <y.yankov6@gmail.com>
2023-11-21 12:02:17 +0200

    Add base pages and nav, style nav, include routing
    Added navigation with Home, About, Contacts, Login, Register and styled
    Installed firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contacts',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.scss'
+})
+export class ContactsComponent {
+
+}
